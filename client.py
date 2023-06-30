8031308867bb45c89dfe9856bea49f8807753ba1
Andrew <it.andr@outlook.com>
2023-06-30 14:41:18 +0300

    Add comment in russian

--- client.py
+++ client.py
@@ -1,19 +1,22 @@
 import socket
 import threading
 
-# Choosing Nickname
+# Choosing Nickname / Ввод псевдонима
 nickname = input("Choose your nickname: ")
 ip_address = "127.0.0.54"
-# Connecting To Server
+# Connecting To Server / Подключение к серверу
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((ip_address, 1067))
 
 # Listening to Server and Sending Nickname
+# Прослушивение сервера и отправка псевдонима
 def receive():
     while True:
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
+            # Получение сообщение от сервера 
+            # Если человек отпаравил свой псевдоним
             message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
                 client.send(nickname.encode('utf-8'))
@@ -21,6 +24,7 @@ def receive():
                 print(message)
         except:
             # Close Connection When Error
+            # Закрытие подключения когда ошибка
             print("An error occured!")
             client.close()
             break
@@ -31,6 +35,7 @@ def write():
         client.send(message.encode('utf-8'))
 
 # Starting Threads For Listening And Writing
+# Начало потока для прослушивание и записывания
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
--- server.py
+++ server.py
@@ -3,32 +3,34 @@ import socket
 import threading
 
 # Connection Data
+# Данные для подключения
 host = '127.0.0.54'
 port = 1067
 
-# Starting Server
+# Starting Server / Запуск сервера
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-# Lists For Clients and Their Nicknames
+# Lists For Clients and Their Nicknames / Списки клиентов и их псевдонимов
 clients = []
 nicknames = []
 
-# Sending Messages To All Connected Clients
+# Sending Messages To All Connected Clients / Отправка сообещения
+# всем подключившимся клиентам
 def broadcast(message):
     for client in clients:
         client.send(message)
 
-# Handling Messages From Clients
+# Handling Messages From Clients / Отправка сообщений от клиентов
 def handle(client):
     while True:
         try:
-            # Broadcasting Messages
+            # Broadcasting Messages / Рассылка сообщений
             message = client.recv(1024)
             broadcast(message)
         except:
-            # Removing And Closing Clients
+            # Removing And Closing Clients / Удаление и закрытие клиентов
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -37,25 +39,25 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
-# Receiving / Listening Function
+# Receiving / Listening Function / Получение и функция прослушивания
 def receive():
     while True:
-        # Accept Connection
+        # Accept Connection / Принятие подключения
         client, address = server.accept()
         print("Connected with {}".format(str(address)))
 
-        # Request And Store Nickname
+        # Request And Store Nickname / Запрос и сохранение псевдонима
         client.send('NICK'.encode('utf-8'))
         nickname = client.recv(1024).decode('utf-8')
         nicknames.append(nickname)
         clients.append(client)
 
-        # Print And Broadcast Nickname
+        # Print And Broadcast Nickname / Вывод и рассылка псевдонима
         print("Nickname is {}".format(nickname))
         broadcast("{} joined!".format(nickname).encode('utf-8'))
         client.send('Connected to server!'.encode('utf-8'))
 
-        # Start Handling Thread For Client
+        # Start Handling Thread For Client / Запуск потока обработки от клиента
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
